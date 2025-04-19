0620f01739342efcbce06fe76ea6603f85688871
renataa <116390061+reeenatamc@users.noreply.github.com>
2025-04-18 22:06:07 -0500

    fixes in views

--- musicapp/views.py
+++ musicapp/views.py
@@ -44,60 +44,6 @@ def login_view(request):
 
     return render(request, 'auth/login.html', {'form': form})
 
-
-# class PromptFormView(FormView):
-#     template_name = 'main_pages/index.html'
-#     form_class = PromptForm
-#     success_url = reverse_lazy('home')  # asegúrate que redirige a la raíz
-#
-#     def form_valid(self, form):
-#
-#         if self.request.user.is_authenticated:
-#             prompt_text = form.cleaned_data['prompt']
-#
-#             user_prompt = UserPrompt.objects.create(user=self.request.user, prompt=prompt_text)
-#
-#             base_output_path = f"media/generated_audio/user_{self.request.user.id}_prompt_{user_prompt.id}"
-#             output_path = f"{base_output_path}_huggingFace.wav"
-#             ac_output_prefix = f"{base_output_path}_audioCraft"
-#
-#             os.makedirs(os.path.dirname(output_path), exist_ok=True)
-#
-#             music_service = MusicService()
-#             song_title = music_service.generate_song_title(prompt_text)
-#
-#             print(song_title)
-#
-#             music_service.generate_music_audiocraft(
-#                 prompts=[prompt_text],
-#                 duration=8,
-#                 output_prefix=ac_output_prefix
-#             )
-#
-#             music_service.generate_music(prompt_text, output_file=output_path)
-#
-#             user_prompt.audio_file = output_path
-#             user_prompt.save()
-#
-#             song_created = SongCreated.objects.create(
-#                 song_name=song_title,  # nombre limitado a 100 caracteres
-#                 audio_file=f"{os.path.relpath(output_path, start=settings.MEDIA_ROOT)}",  # Cambiado
-#                 user=self.request.user,
-#             )
-#
-#             html = Email.dynamic_email(song_created, 'components/music-player.html')
-#             print(html)  # o úsalo como quieras
-#
-#             return super().form_valid(form)
-#         else:
-#             return redirect('login')
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['user_prompts'] = UserPrompt.objects.filter(user=self.request.user)
-#         return context
-
-
 # Clase que maneja el formulario de entrada y la creación de canciones generadas a partir de un prompt
 class PromptFormView(FormView):
     template_name = 'main_pages/index.html'  # Plantilla para renderizar la página
@@ -149,7 +95,7 @@ class PromptFormView(FormView):
             # Crea un registro en SongCreated para almacenar la canción generada
             song_created = SongCreated.objects.create(
                 song_name=song_title,  # Título de la canción (máximo 100 caracteres)
-                audio_file=f"{os.path.relpath(output_path, start=settings.MEDIA_ROOT)}",  # Ruta relativa al archivo de audio
+                audio_file=os.path.relpath(output_path, start=settings.MEDIA_ROOT).replace("\\", "/"),  # Ruta relativa al archivo de audio
                 user=self.request.user,  # Usuario que creó la canción
             )
 
